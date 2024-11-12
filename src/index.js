54eb3d13ecc0dd348e0ac1bc78b75c902036858d
EL5225 <luthfiramadhan155@gmail.com>
2024-11-12 12:58:11 +0700

    feat: add create kamus

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import express from "express"
-import { kamusRoute } from "./routes/kamus.js"
+import express from "express";
+import { kamusRoute } from "./routes/kamus.js";
 
-const PORT = process.env.PORT || 3000
-const app = express()
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
 
 /**
- * TODO : 
+ * TODO :
  * Untuk saat ini hanya endpoint berikut yang perlu dibuat
  * /api/v1/kamus
  * /api/v1/user
  * /api/v1/auth
- * 
+ *
  */
 
-app.use("/api/v1/kamus", kamusRoute)
+app.use("/api/v1/kamus", kamusRoute);
 
 /**
  * Buatin error handlingnya rek
@@ -21,4 +23,4 @@ app.use("/api/v1/kamus", kamusRoute)
  * o iya sekalian buatin handling 404
  */
 
-app.listen(PORT, "0.0.0.0", () => console.log(`Listening ${PORT}`))
+app.listen(PORT, "0.0.0.0", () => console.log(`Listening ${PORT}`));
