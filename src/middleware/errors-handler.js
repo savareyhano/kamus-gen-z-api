5d20dbf2ad3303166d8ef1e8fd8350a34df5d86b
HengkerKucing <140718591+HengkerKucing@users.noreply.github.com>
2024-11-16 19:04:06 +0700

    add & fix : add get started in readme and fix error when import response status on errors-handler.js

---
+++ src/middleware/errors-handler.js
@@ -0,0 +1,37 @@
+import { Prisma } from '@prisma/client'
+import { responseStatus } from '../utils/response.js'
+
+export const pathNotFound = (req, res) => {
+  res.status(404).json({
+    status: responseStatus.NOT_FOUND,
+    message: `cannot find path ${req.originalUrl}`,
+  })
+}
+
+export const serverError = (err, req, res, next) => {
+  if (!err) {
+    return next()
+  }
+
+  if (err instanceof Prisma.PrismaClientInitializationError) {
+    return res.status(500).json({
+      status: responseStatus.SERVER_ERROR,
+      message: 'Gagal Terhubung ke database',
+    })
+  } else if (err instanceof Prisma.PrismaClientRustPanicError) {
+    return res.status(500).json({
+      status: responseStatus.SERVER_ERROR,
+      message: 'Internal Server Error',
+    })
+  } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    return res.status(500).json({
+      status: responseStatus.SERVER_ERROR,
+      message: 'Table tidak ditemukan di Database',
+    })
+  }
+
+  return res.status(500).json({
+    status: responseStatus.SERVER_ERROR,
+    message: 'Ups.., Terjadi Kesalahan Tidak Terduga',
+  })
+}
